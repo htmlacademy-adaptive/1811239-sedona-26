862cc827ceba0f57514832544af7285f54186e82
Nataliy <nk.12k@yandex.ru>
2022-08-27 14:43:30 +0300

    доделала кнопку закрытия и открытия

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -2,6 +2,15 @@ const buttonClose = document.querySelector('.main-nav__close-button');
 const buttonOpen = document.querySelector('.page-header__open-nav-button');
 const menuElement = document.querySelector('.main-nav__list');
 
-buttonOpen.addEventListener('click', => {
-  menuElement.classList.toggle('. main-nav__opened')
+buttonClose.addEventListener('click', () => {
+  //menuElement.classList.toggle('main-nav__close')
+  menuElement.hidden = true;
+  buttonClose.hidden = true;
+  buttonOpen.hidden = false;
+})
+
+buttonOpen.addEventListener('click', () => {
+  menuElement.hidden = false;
+  buttonOpen.hidden = true;
+  buttonClose.hidden = false;
 })
