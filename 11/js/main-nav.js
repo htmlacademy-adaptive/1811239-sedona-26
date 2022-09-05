dad099b4c990b9c1dd391fae32924c0f53a1cb46
Keks <keks@htmlacademy.ru>
2022-09-05 08:59:59 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main-nav.js
@@ -0,0 +1,16 @@
+const buttonClose = document.querySelector('.main-nav__close-button');
+const buttonOpen = document.querySelector('.page-header__open-nav-button');
+const menuElement = document.querySelector('.main-nav__list');
+
+buttonClose.addEventListener('click', () => {
+  //menuElement.classList.toggle('main-nav__close')
+  menuElement.hidden = true;
+  buttonClose.hidden = true;
+  buttonOpen.hidden = false;
+})
+
+buttonOpen.addEventListener('click', () => {
+  menuElement.hidden = false;
+  buttonOpen.hidden = true;
+  buttonClose.hidden = false;
+})
---
+++ 11/js/popup.js
@@ -0,0 +1,22 @@
+const popupOpenbutton = document.querySelector('.button--review');
+const body = document.querySelector('body');
+
+let unlock = true;
+
+if (popupOpenbutton.length > 0) {
+  for (let index = 0; index < popupOpenbutton.length; index++) {
+    const popupOpenbutton = popupOpenbutton[index];
+    popupOpenbutton.addEventListener('click', function (e) {
+      // const popupName = popupOpenbutton.getAttribute('href').replace('#', '');
+      // const curentPopup = document.getElementById(popupName);
+      // popupOpen(curentPopup);
+      // e.preventDefult();
+    });
+  }
+}
+
+// const popupSuccess = document.querySelector('.popup-success');
+// const popupCloseOkButton = document.querySelector('.popup__button');
+// popupCloseOkButton.addEventListener('click', () => {
+//   popupSuccess.hidden = false;
+// })
