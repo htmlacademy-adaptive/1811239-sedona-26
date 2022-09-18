f39a96350c6592cc4e19f435f52b1b3509538a1b
Nataliy <nk.12k@yandex.ru>
2022-09-18 12:42:51 +0300

    Убрала меню в мобильной версии.Добавила Ховер.

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -2,6 +2,10 @@ const buttonClose = document.querySelector('.main-nav__close-button');
 const buttonOpen = document.querySelector('.page-header__open-nav-button');
 const menuElement = document.querySelector('.main-nav__list');
 
+menuElement.hidden = true;
+buttonClose.hidden = true;
+buttonOpen.hidden = false;
+
 buttonClose.addEventListener('click', () => {
   menuElement.hidden = true;
   buttonClose.hidden = true;
